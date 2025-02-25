09f85c6c1d7f2fed3a3fbd6abbc33b0fc7c306fc
fujimogn <fujimogn@gmail.com>
2025-02-26 03:23:10 +0900

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fastify = require('fastify')
 const multipart = require('@fastify/multipart')
-const basicAuth = require('@fastify/basic-auth')
+
 // 環境設定
 const PORT = process.env.PORT || 8080
 const isProduction = process.env.NODE_ENV === 'production'
